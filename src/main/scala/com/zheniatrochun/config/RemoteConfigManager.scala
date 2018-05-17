1411e5fcb38d150ee35d04af3ed0261a82f06ed4
ZheniaTrochun <zheniatrochun@gmail.com>
2018-05-17 17:57:28 +0300

    redis management

--- src/main/scala/com/zheniatrochun/config/RemoteConfigManager.scala
+++ src/main/scala/com/zheniatrochun/config/RemoteConfigManager.scala
@@ -5,24 +5,35 @@ import java.net.URI
 import com.redis.RedisClient
 import com.typesafe.config.Config
 
+
+// simple connector for actor
 private[config] class RemoteConfigManager(val config: Config) {
-  private val redisClient = new RedisClient(new URI(config.getString("redis.url")))
 
   def getRemoteConfig(): Map[String, String] = {
-    redisClient.hgetall1[String, String]("gateway-service-config")
-      .getOrElse(Map.empty[String, String])
+    withRedis { redisClient =>
+      redisClient.hgetall1[String, String]("data-service-config")
+        .getOrElse(Map.empty[String, String])
+    }
   }
 
   def createDummyConfig(): Unit = {
-    val conf = Map("auth" -> "my-auth-service.herokuapp.com",
-      "users" -> "my-data-service.herokuapp.com",
-      "bills" -> "my-data-service.herokuapp.com",
-      "wallets" -> "my-data-service.herokuapp.com")
-
-    redisClient.hmset("gateway-service-config", conf)
+    withRedis { redisClient =>
+      val conf = Map("Sertificate" -> "123")
+      redisClient.hmset("data-service-config", conf)
+    }
   }
 
   def setConfig(entry: (String, String)): Unit = {
-    redisClient.hset("gateway-service-config", entry._1, entry._2)
+    withRedis { redisClient =>
+      redisClient.hset("data-service-config", entry._1, entry._2)
+    }
+  }
+
+  private def withRedis[T](func: (RedisClient) => T): T = {
+    val redisClient = new RedisClient(new URI(config.getString("redis.url")))
+    val res = func(redisClient)
+    redisClient.disconnect
+
+    res
   }
 }
